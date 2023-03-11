d1e1950e967d192c7b3455233eab7d5137e854db
Matheus Kamer <matheus_kamer@hotmail.com>
2023-03-11 19:03:23 -0400

    Entendendo renderização condicional

--- src/Post.js
+++ src/Post.js
@@ -5,10 +5,12 @@ function Post(props) {
     return (
         <>
             <article>
-                <strong>{props.post.title}</strong><br />
+                <strong>
+                    {props.post.read ? <s>{props.post.title}</s> : props.post.title}
+                </strong>
+                <button onClick={() => props.onRemove(props.post.id)}>Remover</button><br />                
                 <small>{props.post.subtitle}</small><br />
-                Media: {props.likes / 2}
-                <p>Desenvolvedor: {props.developer}</p>
+                Media: {props.post.likes / 2}<br />
             </article>
 
             <br />
@@ -17,8 +19,15 @@ function Post(props) {
 }
 
 Post.propTypes = {
-    likes: PropTypes.number.isRequired,
-    post: PropTypes.shape().isRequired
+    
+    onRemove: PropTypes.func.isRequired,
+    post: PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        title: PropTypes.string.isRequired,
+        subtitle: PropTypes.string.isRequired,
+        likes: PropTypes.number.isRequired,
+        read: PropTypes.bool.isRequired
+    }).isRequired
 }
 
 export default Post;
